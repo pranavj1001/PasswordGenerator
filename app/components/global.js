d16a5cbc205b4b656c65d940db6b8ebefe3dfaa0
pranavj1001 <pranavj1001@gmail.com>
2017-06-18 14:11:59 +0530

    add checkBoxes

--- app/components/global.js
+++ app/components/global.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
-import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import { FormGroup, FormControl, Checkbox } from 'react-bootstrap';
 
 class ButtonCompoment extends Component {
 
   render(){
     return(
-      <button className="btn btn-outline-primary">Generate CODE!</button>
+      <div>
+        <form>
+        <div className="form-check">
+            <Checkbox className="form-check-input" type="checkbox" value="">
+             <label className="form-check-label">Small Characters a-z</label>
+            </Checkbox>
+        </div>
+        <div className="form-check">
+            <Checkbox className="form-check-input" type="checkbox" value="">
+             <label className="form-check-label">Capital Characters A-Z</label>
+            </Checkbox>
+        </div>
+        <div className="form-check">
+            <Checkbox className="form-check-input" type="checkbox" value="">
+             <label className="form-check-label">Numbers 0-9</label>
+            </Checkbox>
+        </div>
+        <div className="form-check">
+            <Checkbox className="form-check-input" type="checkbox" value="">
+             <label className="form-check-label">Special Characters</label>
+            </Checkbox>
+        </div>
+        </form>
+        <button className="btn btn-outline-primary generateButton">Generate CODE!</button>
+      </div>
     )
   }
 
