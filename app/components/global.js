2370f0686bdc2fafa18b65e6ca6ae719d6780a1b
pranavj1001 <pranavj1001@gmail.com>
2017-06-18 13:05:42 +0530

    add global Button component

---
+++ app/components/global.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+
+class ButtonCompoment extends Component {
+
+  render(){
+    return(
+      <button className="btn btn-outline-primary">Generate CODE!</button>
+    )
+  }
+
+}
+
+export default ButtonCompoment;
